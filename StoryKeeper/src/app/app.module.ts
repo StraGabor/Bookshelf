3f1c59b18932b1266acc4ebb53d4c3d77fde51d6
Györök Gábor <gyorokgabor8@gmail.com>
2023-04-18 17:48:02 +0200

    Basics for my page
    Components for my pages, Adminguard, models and the menu

--- StoryKeeper/src/app/app.module.ts
+++ StoryKeeper/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { OrderComponent } from './pages/order/order.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SignUpComponent,
+    CartComponent,
+    OrderComponent,
+    ProfileComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
