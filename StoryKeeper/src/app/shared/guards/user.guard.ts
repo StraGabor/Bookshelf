91117c2cfd8cce75be686ec5516b727e4b7055ca
Györök Gábor <gyorokgabor8@gmail.com>
2023-05-05 10:38:20 +0200

    user guard

--- StoryKeeper/src/app/shared/guards/user.guard.ts
+++ StoryKeeper/src/app/shared/guards/user.guard.ts
@@ -5,11 +5,15 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class AdminGuard implements CanActivate {
+export class UserGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      const user = JSON.parse(localStorage.getItem('user') as string);
+      if (user) {
+        return true;
+      }
+    return false;
   }
   
 }
