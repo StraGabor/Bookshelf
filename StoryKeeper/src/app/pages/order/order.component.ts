8c4ffeb8d35a0c4f217be5e2425198a3c0a1a789
Györök Gábor <gyorokgabor8@gmail.com>
2023-05-05 10:14:28 +0200

    mess2

--- StoryKeeper/src/app/pages/order/order.component.ts
+++ StoryKeeper/src/app/pages/order/order.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DocumentReference, doc, getDoc } from 'firebase/firestore';
 import { Book } from 'src/app/shared/models/Book';
 import { BookService } from 'src/app/shared/services/book.service';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { FormBuilder, NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { MatSelect } from '@angular/material/select';
 
 @Component({
   selector: 'app-order',
@@ -12,9 +13,11 @@ import { FormBuilder, NgForm } from '@angular/forms';
 })
 export class OrderComponent implements OnInit {
 
+  @ViewChild('selectType')selectType: MatSelect;
   bookList: Book[] = [];
-  docRef: DocumentReference | undefined;
-  lastBook: Book | undefined;
+  docRef: DocumentReference;
+  lastBook: Book;
+  modify: boolean;
 
   types: string[] = [
     'History',
@@ -23,12 +26,19 @@ export class OrderComponent implements OnInit {
     'Horror',
     'Classic'
   ]
-  editForm: any;
-
+  
   constructor(private afs: AngularFirestore, private bookService: BookService, private fb: FormBuilder) { }
 
+  editForm = new FormGroup({
+    price: new FormControl('',[Validators.required]),
+    title: new FormControl('',[Validators.required]),
+    img: new FormControl('',[Validators.required]),
+    type: new FormControl(''),
+    description: new FormControl('')
+  });
+
   ngOnInit(): void {
-    this.bookService.getAllBook().subscribe((books: any[]) => {
+    this.bookService.getBooks().subscribe((books: any[]) => {
       this.bookList = books.map(i =>{
         const data = i.payload.doc.data() as Book;
         return {
@@ -43,51 +53,84 @@ export class OrderComponent implements OnInit {
     });
   }
 
+  get price(){
+    return this.editForm.get('price');
+  }
+
+  get title(){
+    return this.editForm.get('title');
+  }
+
+  get img(){
+    return this.editForm.get('img');
+  }
+
+  get type(){
+    return this.editForm.get('type');
+  }
+
+  get description(){
+    return this.editForm.get('description');
+  }
+
   clickedRow: any;
   async getGame(book: Book){
     this.docRef = doc(this.afs.firestore,"Books", book.id);
     const docSnap = await getDoc(this.docRef);
     this.lastBook = book;
-    
   }
 
-  del(book: any){
-    this.bookService.deleteBook(book).then(
-      () => console.log("Game deleted!")
-    ).catch(
-      err => console.log(err)
-    )
-  }
-
-  reset(editForm? : NgForm){
-    this.editForm = this.fb.group({
-      title: '',
-      img: '',
-      type: '',
-      price: '',
-      description: '',
-    });
-  }
-
-  onSubmit(editForm: any){
-    let book = editForm.value;
-
-    if (editForm.value.id !== "") {
-      this.bookService.updateBook(book).then(() => {
-        console.log("Book updated!");
-        this.reset();
-      }).catch(err => {
-        console.log(err);
-      });
-    }else{
-      this.bookService.createBook(book).then(() => {
-        console.log("Book saved!");
-        this.reset();
-      }).catch(err => {
-        console.log(err);
-      });
+  onSubmit(){
+    if (!this.editForm.valid) {
+      alert('Not valid!')
+      return;
     }
 
+    let data ={
+      'price': this.editForm.value.price as string,
+      'title' : this.editForm.value.title as string,
+      'img' : this.editForm.value.img as string,
+      'type' : this.selectType?.value,
+      'description' : this.editForm.value.description as string
+    }
+
+    if (!this.modify) {
+      this.bookService.createBook(data);
+    }else{
+      this.bookService.updateBook(this.docRef, data);
+      this.modify = false;
+    }
+  }
+
+  async getBook(book: Book){
+    this.modify = true;
+    this.docRef = doc(this.afs.firestore, "book", book.id);
+    const docSnap = await getDoc(this.docRef);
+    this.lastBook = book;
+    
+    this.editForm = new FormGroup({
+    price: new FormControl('',[Validators.required]),
+    title: new FormControl('',[Validators.required]),
+    img: new FormControl('',[Validators.required]),
+    type: new FormControl(''),
+    description: new FormControl('')
+    })
+    this.clickedRow = book;
+  }
+
+  async deleteBook(book: Book){
+    this.bookService.deleteBook(book);
+  }
+
+  reset(){
+    this.modify = false;
+    this.editForm = new FormGroup({
+      price: new FormControl('',[Validators.required]),
+      title: new FormControl('',[Validators.required]),
+      img: new FormControl('',[Validators.required]),
+      type: new FormControl(''),
+      description: new FormControl('')
+    });
   }
 
 }
--- StoryKeeper/src/app/shared/services/book.service.ts
+++ StoryKeeper/src/app/shared/services/book.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Book } from "../models/Book";
-import { AngularFirestore } from "@angular/fire/compat/firestore";
+import { AngularFirestore, DocumentChangeAction } from "@angular/fire/compat/firestore";
+import { Observable } from 'rxjs';
+import { DocumentReference } from '@angular/fire/firestore';
+import { setDoc } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -12,16 +15,24 @@ editForm: any;
 
   constructor(private afs: AngularFirestore) { }
 
-  getAllBook(){
-    return this.afs.collection<Book>(this.collectionName).valueChanges();
+  getBooks(): Observable<DocumentChangeAction<unknown>[]>{
+    return this.afs.collection(this.collectionName).snapshotChanges();
   }
 
-  updateBook(book: Book){
-    return this.afs.collection<Book>(this.collectionName).doc(book.id).set(book);
+  updateBook(docRef: DocumentReference, book: Book){
+    setDoc(docRef, book).then(
+      () => console.log("Book updated")
+    ).catch(
+      () => console.log("Book update failed!")
+    )
   }
 
-  createBook(book: Book){
-    return this.afs.collection<Book>(this.collectionName).doc(book.id).set(book);
+  createBook(data: Book){
+    this.afs.collection(this.collectionName).add(data).then(
+      () => console.log("Book created!")
+    ).catch(
+      () => console.log("Book creation failed!")
+    )
   }
 
   deleteBook(book: Book){
